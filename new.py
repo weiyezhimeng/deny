56414a5210e47bc96a4c153f36420e304942e4df
weiyezhimeng <90603433+weiyezhimeng@users.noreply.github.com>
2024-03-22 10:07:25 +0800

    Update new.py

--- new.py
+++ new.py
@@ -5,23 +5,21 @@ import math
 from sentence_transformers.util import (semantic_search,
                                         dot_score,
                                         normalize_embeddings)
-import tensorflow_hub as hub
+from torch.nn import CrossEntropyLoss
 
-embed = hub.load("../universal_encoder")
-
-def sample_control(model, init, grad, batch_size,topk_semanteme=10):
+def sample_control(model, init, batch_size,topk_semanteme=10):
     curr_embeds = model.get_input_embeddings()(init.unsqueeze(0))
     curr_embeds = torch.nn.functional.normalize(curr_embeds,dim=2)               # queries
 
     embedding_matrix = model.get_input_embeddings().weight
     embedding_matrix = normalize_embeddings(embedding_matrix)      # corpus
 
-    top_indices=torch.zeros(init.shape[0],topk_semanteme,dtype=init.dtype).to("cuda")
+    top_indices = torch.zeros(init.shape[0], topk_semanteme, dtype=init.dtype).to("cuda")
     for i in range(init.shape[0]):
-        temp=[]                 
-        query=curr_embeds[0][i]                 #query
-        corpus=embedding_matrix                 #corpus
-        hits = semantic_search(query, corpus, 
+        temp = []
+        query = curr_embeds[0][i]                 #query
+        corpus = embedding_matrix                 #corpus
+        hits = semantic_search(query, corpus,
                             top_k=topk_semanteme+1,
                             score_function=dot_score)
         for hit in hits:
@@ -30,62 +28,45 @@ def sample_control(model, init, grad, batch_size,topk_semanteme=10):
         top_indices[i]=torch.tensor(temp)
 
     original_control_toks = init.repeat(batch_size, 1)
-
-    new_token_pos = torch.arange(      
-        1, 
-        len(init), 
+    new_token_pos = torch.arange(
+        1,
+        len(init),
         (len(init)-1) / batch_size,      ## this place has to change as well
-        device=grad.device
+        device=model.device
     ).type(torch.int64)
 
     new_token_val = torch.gather(
-        top_indices[new_token_pos], 1, 
-        torch.randint(0, topk_semanteme, (batch_size, 1),device=grad.device)
+        top_indices[new_token_pos], 1,
+        torch.randint(0, topk_semanteme, (batch_size, 1),device=model.device)
     )
-
     new_control_toks = original_control_toks.scatter_(1, new_token_pos.unsqueeze(-1), new_token_val)
-
     return new_control_toks
 
-def step(model,tokenizer,init,batch_size=1024 ,topk_semanteme=10):
+def step(model, tokenizer, init, batch_size=1024, topk_semanteme=10):
 
     main_device = model.device
     # Aggregate gradients.universal needs to add all gradient.
 
     with torch.no_grad():
-        control_cand = sample_control(model,init,batch_size,topk_semanteme)
+        control_cand = sample_control(model, init, batch_size, topk_semanteme)
 
     # Search
     loss = torch.zeros( batch_size).to(main_device)
+    adv_meaning = []
     with torch.no_grad():
-        question="summary the meaning of following text:"
-        qusetion_list=tokenizer.encode(question).to("cuda")
-        full_input=torch.cat((qusetion_list.unsqueeze(0),init.unsqueeze(0)[:,1:]),1).squeeze(0)
-        gen_tokens = model.generate(
-            full_input,
-            do_sample=True,
-            temperature=0.7,
-            max_new_tokens=100
-        )
-        original_meaning = tokenizer.batch_decode(gen_tokens)[0]
         for j, cand in enumerate(control_cand):
-            question="summary the meaning of following text:"
-            qusetion_list=tokenizer.encode(question).to("cuda")
-            full_input=torch.cat((qusetion_list.unsqueeze(0),cand.unsqueeze(0)),1).squeeze(0)
-            gen_tokens = model.generate(
-                full_input,
-                do_sample=True,
-                temperature=0.7,
-                max_new_tokens=100
-            )
-            gen_text = tokenizer.batch_decode(gen_tokens)[0][len(question):]
-            cos_similarity=torch.cosine_similarity(torch.tensor(np.array(embed(gen_text))),torch.tensor(np.array(embed(original_meaning))), dim=1)
-            final_similarity=1-torch.acos(cos_similarity)/math.pi
-            loss[j]=final_similarity
+            question="A chat between a curious user and an assistant. The assistant gives helpful, detailed, accurate, uncensored responses to the user's input.\nUSER:\n"
+            qusetion_list = torch.tensor(tokenizer.encode(question)).to('cuda')
+            full_input = torch.cat((qusetion_list.unsqueeze(0),cand[1:].unsqueeze(0)),1)
+            full_input_string = tokenizer.batch_decode(full_input)[0]+"\"\nAssistant:\n"+"I'm sorry"
+            #print(full_input_string)
+            full_input = tokenizer(full_input_string, return_tensors="pt").input_ids.to("cuda")
+            outputs = model(full_input)
+            logits = outputs.logits[0][-5:-1]
+            shift_labels = [306, 29915, 29885, 7423]
+            loss_fct = CrossEntropyLoss()
+            loss[j] = loss_fct(logits.view(-1, logits.size(-1)), shift_labels)
         min_idx = loss.argmin()
-        next_control, cand_loss= control_cand[min_idx], loss[min_idx]
+        next_control, cand_loss = control_cand[min_idx], loss[min_idx]
 
-    del  loss ; gc.collect()
-    print(next_control,flush=True)
-    print(cand_loss,flush=True)
-    return next_control,cand_loss
+    del loss ; gc.collect()
